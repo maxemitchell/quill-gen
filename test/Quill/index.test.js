538b8be617d618a5705f521aaeab4acd20e8dcb5
maxemitchell <maxemitchell@gmail.com>
2021-11-07 11:38:18 -0600

    WIP - adding barebones

---
+++ test/Quill/index.test.js
@@ -0,0 +1,9 @@
+import Quill from '../../src/Quill'
+
+describe('Quill Constructor Test', () => {
+    const myQuill = new Quill()
+
+    test('has a petals array', () => {
+        expect(myQuill)
+    })
+})
